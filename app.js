532940b366bfdc21b3fb9618e4d4baf50bd6de8e
Daniele <danielericcelli@me.com>
2023-04-21 18:55:23 +0200

    fix

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express();
 const productRoutes = require('./routes/product');
 const searchRoutes = require('./routes/search');
 const cors = require('cors');
-app.use(cors({origin: '*'}));
-app.use('/api/product', productRoutes);
-app.use('/api/search', searchRoutes);
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use('/api/product', cors(corsOptions), productRoutes);
+app.use('/api/search', cors(corsOptions), searchRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server started');
