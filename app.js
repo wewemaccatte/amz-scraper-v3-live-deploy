c9ffbdea07200bf4e477f52103e36431d22bfd2b
Daniele <danielericcelli@me.com>
2023-04-27 22:18:40 +0200

    remove cors

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const productRoutes = require('./routes/product');
 const searchRoutes = require('./routes/search');
 
-app.use(cors());
+
 app.use('/api/product', productRoutes);
 app.use('/api/search', searchRoutes);
 
